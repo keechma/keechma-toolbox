93a001a4a4e97732eeaf398e92f691e0b181b8c2
Mihael Konjevic <konjevic@gmail.com>
2017-02-23 17:32:46 +0100

    Add route> helper

--- src/cljs/keechma/toolbox/ui.cljs
+++ src/cljs/keechma/toolbox/ui.cljs
@@ -5,5 +5,7 @@
   (deref (ui/subscription ctx subscription args)))
 
 (defn <cmd [ctx command & args]
-  (ui/send-command ctx command args))
+  (apply ui/send-command ctx command args))
 
+(defn route> [ctx]
+  (:data (deref (ui/current-route ctx))))
