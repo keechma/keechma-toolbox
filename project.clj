6ad7a5b415e6205ecd4e990389a32553d44e2f0f
Mihael Konjevic <konjevic@gmail.com>
2018-04-20 12:47:28 +0200

    Update deps and bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject keechma/toolbox "0.1.11"
+(defproject keechma/toolbox "0.1.12"
   :description "Keechma Toolbox"
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.9.946"]
@@ -7,7 +7,7 @@
                  [cljsjs/react-dom "15.6.1-0" :exclusions [cljsjs/react]]
                  [cljsjs/react-dom-server "15.6.1-0" :exclusions [cljsjs/react]]
                  [binaryage/devtools "0.8.2"]
-                 [keechma "0.3.5" :exclusions [cljsjs/react-with-addons]]
+                 [keechma "0.3.6" :exclusions [cljsjs/react-with-addons]]
                  [garden "1.3.2"]
                  [funcool/promesa "1.8.1"]
                  [keechma/forms "0.1.2"]
