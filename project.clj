1360409ffd8223ecf3d626bb037c1f9bedea118c
Mihael Konjevic <konjevic@gmail.com>
2018-10-09 16:57:05 +0200

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject keechma/toolbox "0.1.18"
+(defproject keechma/toolbox "0.1.19"
   :description "Keechma Toolbox"
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.339"]
